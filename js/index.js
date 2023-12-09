75e31c013e3ba04866c9c10d645608cc16a5163f
maeciosso <mauriciocastelao2006@gmail.com>
2023-12-09 20:30:33 -0300

    media queris e pequenos ajustes

--- js/index.js
+++ js/index.js
@@ -3,3 +3,19 @@ document.getElementById('menu').addEventListener('click', () => {
     let menu = document.getElementById('divMenu')
     menu.classList.toggle('hide')
 })
+
+//contatos
+document.getElementById('contato').addEventListener('click', () => {
+    let cont = document.getElementById('dContatos')
+    cont.classList.toggle('hide')
+})
+
+//menu
+document.getElementById('proje').addEventListener('click', () => {
+    let menu = document.getElementById('divMenu')
+    menu.classList.toggle('hide')
+})
+document.getElementById('sob').addEventListener('click', () => {
+    let menu = document.getElementById('divMenu')
+    menu.classList.toggle('hide')
+})
