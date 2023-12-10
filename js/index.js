80fec0bf2ee17bfe8aba4a78782230b214eccf5a
maeciosso <mauriciocastelao2006@gmail.com>
2023-12-10 09:26:31 -0300

    testes

--- js/index.js
+++ js/index.js
@@ -1,21 +1,29 @@
-//menu
-document.getElementById('menu').addEventListener('click', () => {
-    let menu = document.getElementById('divMenu')
-    menu.classList.toggle('hide')
-})
+document.getElementById('menu').addEventListener('click', (event) => {
+    event.stopPropagation();
+    let menu = document.getElementById('divMenu');
+    menu.classList.toggle('hide');
+});
 
-//contatos
-document.getElementById('contato').addEventListener('click', () => {
-    let cont = document.getElementById('dContatos')
-    cont.classList.toggle('hide')
-})
+document.getElementById('x').addEventListener('click', (event) => {
+    event.stopPropagation();
+    let menu = document.getElementById('divMenu');
+    menu.classList.toggle('hide');
+});
 
-//menu
-document.getElementById('proje').addEventListener('click', () => {
-    let menu = document.getElementById('divMenu')
-    menu.classList.toggle('hide')
-})
-document.getElementById('sob').addEventListener('click', () => {
-    let menu = document.getElementById('divMenu')
-    menu.classList.toggle('hide')
-})
+document.getElementById('contato').addEventListener('click', (event) => {
+    event.stopPropagation();
+    let cont = document.getElementById('dContatos');
+    cont.classList.toggle('hide');
+});
+
+document.getElementById('proje').addEventListener('click', (event) => {
+    event.stopPropagation();
+    let menu = document.getElementById('divMenu');
+    menu.classList.toggle('hide');
+});
+
+document.getElementById('sob').addEventListener('click', (event) => {
+    event.stopPropagation();
+    let menu = document.getElementById('divMenu');
+    menu.classList.toggle('hide');
+});
